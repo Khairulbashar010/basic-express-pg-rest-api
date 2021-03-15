ec86302d5f1446305a8a63b771aca745a80bec45
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-03-15 22:37:37 +0600

    Deprecation handled

--- PGexpress/db.js
+++ PGexpress/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "root",
+    password: "",
     database: "postgres",
     host: "localhost",
     port: 5432
